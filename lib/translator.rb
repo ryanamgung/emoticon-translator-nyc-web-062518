a7c9964f0f58b01cf8ae6479b3fa846f89237df7
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-22 00:57:08 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,7 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emote)
-  if load_library(file_path) 
+  if load_library(file_path)["get_emoticon"][]
 end
 
 def get_english_meaning
